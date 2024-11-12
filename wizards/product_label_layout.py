f37388d8172cae06aec1b1574734578d415f37a8
jwaes <jaco.waes@gmail.com>
2024-11-12 14:18:20 +0000

    df

--- wizards/product_label_layout.py
+++ wizards/product_label_layout.py
@@ -3,16 +3,16 @@ from odoo import models, fields
 class ProductLabelLayout(models.TransientModel):
     _inherit = 'product.label.layout'
 
-    label_type = fields.Selection(
+    print_format = fields.Selection(
         selection_add=[('99x67_2x4', '2 x 4 (99.1 x 67.7 mm)')],
         ondelete={'99x67_2x4': 'set default'},
     )
 
     def _get_label_layout(self):
-        res = super(ProductLabelLayout, self)._get_label_layout()
+        res = super()._get_label_layout()
         res.update({
             '99x67_2x4': {
-                'paperformat': 'label_99x67_2x4',
+                'paperformat': 'paperformat_label_99x67_2x4',
                 'rows': 4,
                 'cols': 2,
                 'height': 67.7,
