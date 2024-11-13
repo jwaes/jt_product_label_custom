9e0c955993cae10722728eeb9c12383128b69117
jwaes <jaco.waes@gmail.com>
2024-11-13 16:05:36 +0000

    lab10

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Product',
-    'version': '0.15',
+    'version': '0.16',
 
     # any module necessary for this one to work correctly
     'depends': ['product'],
